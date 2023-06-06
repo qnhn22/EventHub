4220cd82be48b27ab59b51c80e18dc6cbc0a0b14
qnhn2 <nhatquan234@gmail.com>
2023-06-06 14:36:49 +0700

    build model association and update controllers

--- app/models/user_attendance.rb
+++ app/models/user_attendance.rb
@@ -1,5 +1,5 @@
 class UserAttendance < ApplicationRecord
-    enum status: { invited: 0, accepted: 1 }
     belongs_to :attendee, foreign_key: "user_id", class_name: "User"
     belongs_to :attended_event, foreign_key: "event_id", class_name: "Event"
+
 end
